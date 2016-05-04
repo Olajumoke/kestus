e97e7b557b222df8e2174df1a207be0a2ad1d9b2
olajumoke <olajumoke.ca@gmail.com>
2016-05-04 16:03:53 +0100

    Modified templates, views and urls

--- general/views.py
+++ general/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import JobOpenings
+from django.utils import timezone
 
 # Create your views here.
+
+def job_list(request):
+    job_list = JobOpenings.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'general/job_list.html', {'job_list': job_list})
